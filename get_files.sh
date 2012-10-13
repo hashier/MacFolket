90193458d0cb42e385803a184867cf0c42e7d364
Christopher Loessl <cloessl@x-berg.de>
2012-10-13 05:51:20 +0200

    Changed to curl
    from wget

--- get_files.sh
+++ get_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-wget -c http://folkets-lexikon.csc.kth.se/folkets/folkets_en_sv_public.xml
-wget -c http://folkets-lexikon.csc.kth.se/folkets/folkets_sv_en_public.xml
+echo "Getting files"
+
+curl -C - -sS -o folkets_sv_en_public.xml http://folkets-lexikon.csc.kth.se/folkets/folkets_en_sv_public.xml
+ curl -C - -sS -o folkets_en_sv_public.xml http://folkets-lexikon.csc.kth.se/folkets/folkets_sv_en_public.xml
+
+ echo "Got all files"
+
