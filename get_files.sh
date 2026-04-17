375c48f75b2a9ba9e3803c0e39c78f85ec575060
Christopher Loessl <cloessl+github@gmail.com>
2026-04-17 15:53:36 +0200

    refactor: replace wget with curl
    curl ships with macOS, wget doesn't. Removes an external dependency.

--- get_files.sh
+++ get_files.sh
@@ -2,8 +2,8 @@
 
 echo "Getting files"
 
-wget -c http://folkets-lexikon.csc.kth.se/folkets/folkets_en_sv_public.xml
-wget -c http://folkets-lexikon.csc.kth.se/folkets/folkets_sv_en_public.xml
+curl -fLO http://folkets-lexikon.csc.kth.se/folkets/folkets_en_sv_public.xml
+curl -fLO http://folkets-lexikon.csc.kth.se/folkets/folkets_sv_en_public.xml
 
 echo "Got all files"
 
