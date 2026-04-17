e8603eb7dc62db5629835a1ca77d77240021413a
Christopher Loessl <cloessl+github@gmail.com>
2026-04-17 16:48:11 +0200

    fix: use curl -C - to skip fully downloaded files
    curl exit code 33 means the file is already complete (server
    returned 416 Range Not Satisfiable). Matches wget -c behavior.

--- get_files.sh
+++ get_files.sh
@@ -2,8 +2,11 @@
 
 echo "Getting files"
 
-curl -fLO http://folkets-lexikon.csc.kth.se/folkets/folkets_en_sv_public.xml
-curl -fLO http://folkets-lexikon.csc.kth.se/folkets/folkets_sv_en_public.xml
+curl -C - -fLO http://folkets-lexikon.csc.kth.se/folkets/folkets_en_sv_public.xml
+rc=$?; [ $rc -eq 0 ] || [ $rc -eq 33 ] || exit $rc
+
+curl -C - -fLO http://folkets-lexikon.csc.kth.se/folkets/folkets_sv_en_public.xml
+rc=$?; [ $rc -eq 0 ] || [ $rc -eq 33 ] || exit $rc
 
 echo "Got all files"
 
